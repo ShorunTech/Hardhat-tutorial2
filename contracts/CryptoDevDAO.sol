041cb930cfcc1c445b203345b8b9a1e8887b0fb0
Shoronke Gbenga <gshoronke@gmail.com>
2022-12-13 16:30:10 +0100

    CryptoDevDAO

--- contracts/CryptoDevDAO.sol
+++ contracts/CryptoDevDAO.sol
@@ -36,4 +36,9 @@ interface ICryptoDevsNFT {
     /// @return Returns the number of NFTs owned
     function balanceOf(address owner) external view returns (uint256);
 
+ /// @dev tokenOfOwnerByIndex returns a tokenID at given index for owner
+    /// @param owner - address to fetch the NFT TokenID for
+    /// @param index - index of NFT in owned tokens array to fetch
+    /// @return Returns the TokenID of the NFT
 
+    
