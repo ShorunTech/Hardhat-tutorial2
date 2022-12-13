463d5894678eae06dff0115090b515db087922c0
Shoronke Gbenga <gshoronke@gmail.com>
2022-12-13 16:16:07 +0100

    CryptoDevDAO

--- contracts/CryptoDevDAO.sol
+++ contracts/CryptoDevDAO.sol
@@ -4,3 +4,8 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 // We will add the Interfaces here
+
+contract CryptoDevsDAO is Ownable {
+    // We will write contract code here
+}
+
