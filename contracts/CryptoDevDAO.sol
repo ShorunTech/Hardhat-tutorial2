790c0919516a9b3ff8e76b601edf29610e8f8e43
Shoronke Gbenga <gshoronke@gmail.com>
2022-12-18 20:58:01 +0100

    cryptodevdao

--- contracts/CryptoDevDAO.sol
+++ contracts/CryptoDevDAO.sol
@@ -164,10 +164,7 @@ contract CryptoDevsDAO is Ownable {
         proposal.executed = true;
     }
 
-     /// @dev withdrawEther allows the contract owner (deployer) to withdraw the ETH from the contract
-    function withdrawEther() external onlyOwner {
-        payable(owner()).transfer(address(this).balance);
-    }
+    
 
    // The following two functions allow the contract to accept ETH deposits
     // directly from a wallet without calling a function
