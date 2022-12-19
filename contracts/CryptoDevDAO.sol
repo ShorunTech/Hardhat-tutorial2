a9e184b7c220eec3471d1823160043f8e28f78b9
Shoronke Gbenga <gshoronke@gmail.com>
2022-12-19 15:43:28 +0100

    cryptoDevdao

--- contracts/CryptoDevDAO.sol
+++ contracts/CryptoDevDAO.sol
@@ -3,9 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-/**
- * Interface for the FakeNFTMarketplace
- */
+
 interface IFakeNFTMarketplace {
     /// @dev getPrice() returns the price of an NFT from the FakeNFTMarketplace
     /// @return Returns the price in Wei for an NFT
