577524ff989c33ad5d0fa000592f25717c5a35e8
Shoronke Gbenga <gshoronke@gmail.com>
2022-12-24 15:21:50 +0100

    Cryptodevdao

--- contracts/CryptoDevDAO.sol
+++ contracts/CryptoDevDAO.sol
@@ -66,15 +66,13 @@ contract CryptoDevsDAO is Ownable {
     IFakeNFTMarketplace nftMarketplace;
     ICryptoDevsNFT cryptoDevsNFT;
 
-    // Create a payable constructor which initializes the contract
-    // instances for FakeNFTMarketplace and CryptoDevsNFT
-    // The payable allows this constructor to accept an ETH deposit when it is being deployed
-    constructor(address _nftMarketplace, address _cryptoDevsNFT) payable {
-        nftMarketplace = IFakeNFTMarketplace(_nftMarketplace);
-        cryptoDevsNFT = ICryptoDevsNFT(_cryptoDevsNFT);
-    }
+   
 
-  
+    // Create an enum named Vote containing possible options for a vote
+        enum Vote {
+        YAY, // YAY = 0
+        NAY // NAY = 1
+    }
 
     function createProposal(uint256 _nftTokenId)
         external
