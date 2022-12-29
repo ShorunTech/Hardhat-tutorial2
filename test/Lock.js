e711b840abeb524350d18f7851e234b645d91a52
Shoronke Gbenga <gshoronke@gmail.com>
2022-12-29 16:45:57 +0100

    Lock.js

--- test/Lock.js
+++ test/Lock.js
@@ -116,7 +116,9 @@ describe("Lock", function () {
 
         await time.increaseTo(unlockTime);
 
-    
+        await expect(lock.withdraw()).to.changeEtherBalances(
+          [owner, lock],
+          [lockedAmount, -lockedAmount]
         );
       });
     });
