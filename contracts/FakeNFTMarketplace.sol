0afb42f58efb80c2c14999eca80c5b7f8a1f0c69
Shoronke Gbenga <gshoronke@gmail.com>
2022-12-29 16:43:36 +0100

    FakeNFTMarketplace.sol

--- contracts/FakeNFTMarketplace.sol
+++ contracts/FakeNFTMarketplace.sol
@@ -14,7 +14,10 @@ contract FakeNFTMarketplace {
         tokens[_tokenId] = msg.sender;
     }
 
- 
+    /// @dev getPrice() returns the price of one NFT
+    function getPrice() external view returns (uint256) {
+        return nftPrice;
+    }
 
     /// @dev available() checks whether the given tokenId has already been sold or not
     /// @param _tokenId - the tokenId to check for
