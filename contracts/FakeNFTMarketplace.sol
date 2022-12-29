bf581741227d4bb850c35a66fe2c86c89c514234
Shoronke Gbenga <gshoronke@gmail.com>
2022-12-29 16:40:47 +0100

    fakenftmarketplace.sol

--- contracts/FakeNFTMarketplace.sol
+++ contracts/FakeNFTMarketplace.sol
@@ -24,6 +24,9 @@ contract FakeNFTMarketplace {
     function available(uint256 _tokenId) external view returns (bool) {
         // address(0) = 0x0000000000000000000000000000000000000000
         // This is the default value for addresses in Solidity
-       
+        if (tokens[_tokenId] == address(0)) {
+            return true;
+        }
+        return false;
     }
 }
