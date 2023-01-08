48a40638bd34db6a2f3ec9ec9cc6874902860c9b
Shoronke Gbenga <gshoronke@gmail.com>
2023-01-08 21:52:41 +0100

    lock.js

--- test/Lock.js
+++ test/Lock.js
@@ -9,10 +9,10 @@ describe("Lock", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
-    const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-    
+ 
 
+
+  
     const lockedAmount = ONE_GWEI;
     const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
 
